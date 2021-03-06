988d21b991dd21c81f1b743ede68ff16507393de
joseph <joseph.2210@hotmail.es>
2021-03-06 16:05:31 -0500

    delete

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ cliente = pymongo.MongoClient(
 baseDatos = cliente[MONGO_BASEDATOS]
 # desde dentro de la base de datos accedo a mi coleccion
 coleccion = baseDatos[MONGO_COLECCION]
-ID_ALUMNO=""
+ID_ALUMNO = ""
 
 
 def mostrarDatos():
@@ -61,18 +61,63 @@ def crearRegistro():
         messagebox.showerror(message="Los campos no pueden estar vacios")
     mostrarDatos()
 
+
 def dobleCickTabla(event):
-    global ID_ALUMNO #ponemos el ID_ALUMNO como global
-    ID_ALUMNO = str(tabla.item(tabla.selection())["text"]) #le damos a ID_Alumno el id del elemento seleccionado
-    #print(ID_ALUMNO)
-    documento=coleccion.find({"_id":ObjectId(ID_ALUMNO)})[0] #obtenemos el _id
-    #Accedemos a los datos para guardarlos
-    nombre.delete(0,END)
-    nombre.insert(0,documento["nombre"])
-    sexo.delete(0,END)
-    sexo.insert(0,documento["sexo"])
-    calificacion.delete(0,END)
-    calificacion.insert(0,documento["calificacion"])
+    global ID_ALUMNO  # ponemos el ID_ALUMNO como global
+    # le damos a ID_Alumno el id del elemento seleccionado
+    ID_ALUMNO = str(tabla.item(tabla.selection())["text"])
+    # print(ID_ALUMNO)
+    documento = coleccion.find({"_id": ObjectId(ID_ALUMNO)})[
+                               0]  # obtenemos el _id
+    # Accedemos a los datos para guardarlos
+    nombre.delete(0, END)
+    nombre.insert(0, documento["nombre"])
+    sexo.delete(0, END)
+    sexo.insert(0, documento["sexo"])
+    calificacion.delete(0, END)
+    calificacion.insert(0, documento["calificacion"])
+    crear["state"] = "disabled"
+    editar["state"] = "normal"
+    borrar["state"] = "normal"
+
+
+def editarRegistro():
+    global ID_ALUMNO
+    # Validamos que los campos no esten vacios
+    if len(nombre.get()) != 0 and len(sexo.get()) != 0 and len(calificacion.get()) != 0:
+        try:
+            idBuscar = {"_id": ObjectId(ID_ALUMNO)}
+            nuevosValores = {"nombre": nombre.get(
+            ), "sexo": sexo.get(), "calificacion": calificacion.get()}
+            coleccion.update(idBuscar, nuevosValores)
+            nombre.delete(0, END)
+            sexo.delete(0, END)
+            calificacion.delete(0, END)
+        except pymongo.errors.ConnectionFailure as error:
+            print(error)
+    else:
+        messagebox.showerror("Los campos no pueden estar vacios")
+    mostrarDatos()
+    crear["state"] = "normal"
+    editar["state"] = "disabled"
+    borrar["state"] = "disabled"
+
+def borrarRegistro():
+    global ID_ALUMNO
+
+    try:
+        #Pasamos el id a buscar
+        idBuscar={"_id":ObjectId(ID_ALUMNO)}
+        coleccion.delete_one(idBuscar)
+        nombre.delete(0, END)
+        sexo.delete(0, END)
+        calificacion.delete(0, END)
+    except pymongo.errors.ConnectionFailure as error:
+        print(error)
+    mostrarDatos()
+    crear["state"] = "normal"
+    editar["state"] = "disabled"
+    borrar["state"] = "disabled"
 
 ventana = Tk()  # crear una ventana
 tabla = ttk.Treeview(ventana, columns=2)  # Creamos una tabla en forma de arbol
@@ -80,7 +125,7 @@ tabla = ttk.Treeview(ventana, columns=2)  # Creamos una tabla en forma de arbol
 tabla.grid(row=1, column=0, columnspan=2)  # definimos la posicion de la tabla
 tabla.heading("#0", text="ID")  # cabezeras
 tabla.heading("#1", text="NOMBRE")  # cabezeras
-#Registrar el click
+# Registrar el click
 tabla.bind("<Double-Button-1>", dobleCickTabla)
 
 # Entrys
@@ -98,11 +143,18 @@ calificacion = Entry(ventana)
 calificacion.grid(row=4, column=1)
 # Boton crear
 crear = Button(ventana, text="Crear alumno",
-               command=crearRegistro, bg="green", fg="white")
+               command=crearRegistro, bg="#8aed3a", fg="black")
 crear.grid(row=5, columnspan=2)
 
-#Boton editar
-editar=Button(ventana,text="Editar alumbo", command=editarRegistro)
+# Boton editar
+editar = Button(ventana,text="Editar alumno", command=editarRegistro, bg="yellow", fg="black")
+editar.grid(row=6, columnspan=2)
+editar["state"] = "disable"
+
+# Boton borrar
+borrar = Button(ventana,text="Borrar alumno", command=borrarRegistro, bg="#ff0029", fg="white")
+borrar.grid(row=7, columnspan=2)
+borrar["state"] = "disable"
 
 mostrarDatos()
 ventana.mainloop()  # ciclo principal, la ventana va a estar tomando el control del programa
